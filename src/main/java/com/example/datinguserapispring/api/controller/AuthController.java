31aca61fbdc7ef45ecf63d6793f23bdd8224807e
Maksim9999-123 <aslanyan.maksim@gmail.com>
2024-06-25 12:53:20 +0400

    add README.md

--- src/main/java/com/example/datinguserapispring/api/controller/AuthController.java
+++ src/main/java/com/example/datinguserapispring/api/controller/AuthController.java
@@ -21,6 +21,9 @@ public class AuthController {
     public AuthResponse login(@RequestBody AuthAppleRequest request) {
         String var = "poxos";
         String var1 = "poxos";
+        String var3 = "poxos";
+        String var4 = "poxos";
+
         return authService.loginApple(request);
     }
 
